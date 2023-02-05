1034e4f7fdcac1e17b4ba4fe23c453ea10dcb9c3
D L <mass17@pm.me>
2023-02-05 12:15:12 +0300

    05.02

--- Tic-tac toe.py
+++ Tic-tac toe.py
@@ -5,60 +5,71 @@ dashes = 13
 spaces = 14
 counter = 0
 is_win = False
+tictoken = ''
 
 
 def check_win():
     win_coords = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
     for each in win_coords:
-        if booard[each[0]] == board[aech[1]] == board[each[one]]:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
             return board[each[0]]
     return False
-def draw_board():
 
-   for i in range(cells):
+def make_step(count):
+    if count % 2 == 0:
+        token = take_input('X')
+    else:
+        token = take_input('0')
+    return token
+
+
+def draw_board():
+    for i in range(cells):
        print(' ' * spaces, end="")
        print('-' * dashes)
        print(' ' * spaces, end="")
        print(f'! {board[0 + i * 3]} ! {board[1 + i * 3]} ! {board[2 + i * 3]} !')
        print(' ' * spaces, end="")
        print("-" * dashes)
-def take_input(x):
-    return player_answer
 
-while not is_win:
-   draw_board()
+def take_input(player_token):
+    player_answer = input(f'Where we put a {player_token}?: ')
+    if player_answer == "":
+        return 1
+    elif int(player_answer) >= len(board):
+        print("this area is not available")
+        return 1
+    player_answer = int(player_answer)
+    if str(board[player_answer]) not in 'XO':
+        board[player_answer] = player_token
+    else:
+        print("This cell is already taken!")
+        return 1
+    return player_token
+
+def main():
+    counter = 0
+    is_win = False
+
+    while not is_win:
+        draw_board()
+
+        tictoken = make_step(counter)
+
+        counter+=1
+
+        if counter > 4:
+            winner = check_win()
+            if winner:
+                is_win = True
+                print(f'{tictoken} is win!')
+            elif counter == 9:
+                print("Draw! You're amazing!")
+                return 1
+    draw_board()
+    input('Press enter to exit.')
+
+main()
 
 
-   if counter % 2 == 0:
-       player_token = 'X'
-   else:
-       player_token = '0'
-
-   player_answer = input(f'Where we put a {player_token}?: ')
-   if player_answer == "":
-       break
-   elif  int(player_answer) >= len(board):
-       print("this area is not available")
-       continue
-
-   player_answer = int(player_answer)
-
-
-   if str(board[player_answer]) not in 'XO':
-       board[player_answer] = player_token
-   else:
-       print("This cell is already taken!")
-       continue
-   counter += 1
-
-   if counter > 4:
-       for each in win_coords:
-           if board[each[0]] == board[each[1]] == board[each[2]]:
-               is_win = True
-               break
-       if is_win:
-           print(f'{player_token} is win!')
-   if counter == 9:
-       print("Draw! You're amazing!")
-       break
 
