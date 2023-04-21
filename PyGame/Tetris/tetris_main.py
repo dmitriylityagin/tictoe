3e4c97318cde6344be691d508354181df1d0bed5
D L <mass17@pm.me>
2023-04-21 12:12:43 +0300

    ok

---
+++ PyGame/Tetris/tetris_main.py
@@ -0,0 +1,50 @@
+import pygame as pg
+from random import choice, randrange
+from copy import deepcopy
+
+W, H = 10, 20
+TILE = 40
+GAME_RES = W * TILE, H * TILE
+RES = 750, 940
+FPS = 60
+
+pg.init()
+game_sc = pg.display.set_mode(GAME_RES)
+# game_sc = pg.Surface(GAME_RES)
+clock = pg.time.Clock()
+
+grid = [pg.Rect(x * TILE, y * TILE, TILE, TILE) for x in range(W) for y in range(H)]
+
+figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
+               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
+               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
+               [(0, 0), (-1, 0), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
+
+figures = [[pg.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
+figure_rect = pg.Rect(0, 0, TILE - 2, TILE - 2)
+
+figure = figures[0]
+
+
+
+while True:
+
+    game_sc.fill(pg.Color('green'))
+
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+    #draw grid
+    [pg.draw.rect(game_sc, (40, 40, 40), i_rect, 1) for i_rect in grid]
+
+    #draw figure
+    for i in range(4):
+        figure_rect.x = figure[i].x * TILE
+        figure_rect.y = figure[i].y * TILE
+        pg.draw.rect(game_sc, pg.Color('white'), figure_rect)
+    pg.display.flip()
+    clock.tick(FPS)
