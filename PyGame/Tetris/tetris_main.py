d9eaaf576d96be5583ec579b3094103eb2e9310d
D L <mass17@pm.me>
2023-04-22 13:02:32 +0300

    ok

--- PyGame/Tetris/tetris_main.py
+++ PyGame/Tetris/tetris_main.py
@@ -3,7 +3,7 @@ from random import choice, randrange
 from copy import deepcopy
 
 W, H = 10, 20
-TILE = 40
+TILE = 35
 GAME_RES = W * TILE, H * TILE
 RES = 750, 940
 FPS = 60
@@ -26,18 +26,53 @@ figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
 figures = [[pg.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
 figure_rect = pg.Rect(0, 0, TILE - 2, TILE - 2)
 
-figure = figures[0]
+anim_count, anim_speed, anim_limit = 0, 60, 2000
 
+figure = deepcopy(figures[3])
+
+def check_borders():
+    if figure[i].x < 0 or figure[i].x > W - 1:
+        return False
+    if figure[i].y > H - 1:
+        return False
+
+    return True
 
 
 while True:
-
+    dx = 0
     game_sc.fill(pg.Color('green'))
 
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                dx = -1
+            elif event.key == pg.K_RIGHT:
+                dx = 1
+            elif event.key == pg.K_DOWN:
+                anim_limit = 100
+    #move x
+    figure_old = deepcopy(figure)
+    for i in range(4):
+        figure[i].x += dx
+        if not check_borders():
+            figure = deepcopy(figure_old)
+            break
+    #move y
+    anim_count += anim_speed
+    if anim_count > anim_limit:
+        anim_count = 0
+        figure_old = deepcopy(figure)
+        for i in range(4):
+            figure[i].y += 1
+            if not check_borders():
+                figure = deepcopy(figure_old)
+                anim_limit = 2000
+                break
+
     #draw grid
     [pg.draw.rect(game_sc, (40, 40, 40), i_rect, 1) for i_rect in grid]
 
